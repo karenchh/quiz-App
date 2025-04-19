590198b3c7b775df53e7971a04d3a2ad70c2a79d
Karen Chahrour <karenchahrour123@gmail.com>
2025-04-20 01:36:06 +0300

    Stopping the page from reload

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,6 +3,8 @@ const loginForm = document.getElementById("form-login");
 loginForm.addEventListener("submit", loginfunction);
 
 function loginfunction(event){
+
+    event.preventDefault(); // stop page reload
     const email = document.getElementById("login-email");
     const password = document.getElementById("login-password");
 
