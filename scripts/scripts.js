e0f53cde1a9a236e1b940a9e14ac2174e1e5b5ae
Karen Chahrour <karenchahrour123@gmail.com>
2025-04-20 01:35:10 +0300

    Retrieving the password from html entered by the user

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,5 +4,6 @@ loginForm.addEventListener("submit", loginfunction);
 
 function loginfunction(event){
     const email = document.getElementById("login-email");
+    const password = document.getElementById("login-password");
 
 }
