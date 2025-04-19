3c1e2a7fc7092541617737029c7fcd49209d9d81
Karen Chahrour <karenchahrour123@gmail.com>
2025-04-20 01:40:55 +0300

    Adding the admin as a hardcopy to the local storage

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,9 @@
+const adminuser = {
+    "email" : "admin@quiz.com",
+    "password" : "admin123"
+}
+
+localStorage.setItem("admin",JSON.stringify(adminuser));
 
 const loginForm = document.getElementById("form-login");
 loginForm.addEventListener("submit", loginfunction);
@@ -7,5 +13,6 @@ function loginfunction(event){
     event.preventDefault(); // stop page reload
     const email = document.getElementById("login-email");
     const password = document.getElementById("login-password");
+
 
 }
