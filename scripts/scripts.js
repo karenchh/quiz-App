053cf7fbe4cb4e704aad2de23b4ad680396adaaf
Karen Chahrour <karenchahrour123@gmail.com>
2025-04-20 02:01:17 +0300

    Checking if the user is admin

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,7 +3,8 @@ const adminuser = {
     "password" : "admin123"
 }
 
-localStorage.setItem("admin",JSON.stringify(adminuser));
+localStorage.setItem("users",JSON.stringify(adminuser));
+localStorage.removeItem("admin")
 
 const loginForm = document.getElementById("form-login");
 loginForm.addEventListener("submit", loginfunction);
@@ -11,8 +12,15 @@ loginForm.addEventListener("submit", loginfunction);
 function loginfunction(event){
 
     event.preventDefault(); // stop page reload
-    const email = document.getElementById("login-email");
-    const password = document.getElementById("login-password");
+    const email = document.getElementById("login-email").value;
+    const password = document.getElementById("login-password").value;
 
+    //check for login admin
+    if(email === "admin@quiz.com" && password === "admin123"){
+        window.location.href = '../pages/dashboard.html'; // redirect to admin dashboard 
+        return;
+    }
 
+    const users = localStorage(JSON.parse("users"));
+    console.log(users)
 }
