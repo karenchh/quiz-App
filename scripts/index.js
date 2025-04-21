0a9ee63acb644508c969fd75437d515592bafc3d
Karen Chahrour <karenchahrour123@gmail.com>
2025-04-21 16:47:17 +0300

    Create a quizdev for each and assigning to it the class and id)

--- scripts/index.js
+++ scripts/index.js
@@ -54,5 +54,8 @@ const quizzes = [
   localStorage.setItem('quizzes', JSON.stringify(quizzes));
   for (let i = 0; i < quizzes.length; i++) {
     const quiz = quizzes[i]; // save each quiz in quiz variable
-
+    //Create a quizdev for each and assigning to it the class="quiz" and id="quiz-n(i+1)"
+    const quizdiv = document.createElement('div');
+    quizdiv.classList.add('quiz');
+    quizdiv.id = `quiz-n${i + 1}`;
     }
