dbe1f73686ed9c50bde88786873064fe078cdf06
Karen Chahrour <karenchahrour123@gmail.com>
2025-04-21 19:19:44 +0300

    Adding quizn2 to the localstorage

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -23,7 +23,47 @@ const quiz1 = [
       ];
       
 localStorage.setItem("quizn1", JSON.stringify(quiz1));
+
+const quiz2 = [
+    {
+      question: "What is JavaScript used for?",
+      options: ["Data storage", "Web Interactivity", "Database management"],
+      correctAnswer: "Web Interactivity"
+    },
+    {
+      question: "Which keyword is used to declare a variable?",
+      options: ["var", "define", "int"],
+      correctAnswer: "var"
+    },
+    {
+      question: "Which company developed JavaScript?",
+      options: ["Microsoft", "Netscape", "Google"],
+      correctAnswer: "Netscape"
+    }
+  ];
+localStorage.setItem("quizn2", JSON.stringify(quiz2));
+
+const quiz3 = [
+    {
+      question: "What is the correct file extension for Python files?",
+      options: [".py", ".pt", ".pyt"],
+      correctAnswer: ".py"
+    },
+    {
+      question: "Which keyword is used to define a function in Python?",
+      options: ["function", "def", "fun"],
+      correctAnswer: "def"
+    },
+    {
+      question: "Which data structure is ordered and changeable?",
+      options: ["Tuple", "Set", "List"],
+      correctAnswer: "List"
+    }
+  ];
+localStorage.setItem("quizn3", JSON.stringify(quiz3));
+  
+
 const quiznumber = new URLSearchParams(window.location.search); // to bring it from the url
 const quizid = quiznumber.get("id");
-console.log(quizid)
+//console.log(quizid);
 
