ebbe221159fd81387135fa7ebacc9ae0174802ba
Karen Chahrour <karenchahrour123@gmail.com>
2025-04-21 16:27:27 +0300

    Adding the css and sql quizzes objects including info to the quizzes array

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -93,29 +93,45 @@ const quizzes = [
         text: 'Quiz',
         link: '../pages/quiz.html?id=quiz-n1'
       } // ?id=quiz-n1 query parameter to pass it to the destination page to know in the next page which quiz is chosen
-    }
-    ,{
+    },
+    {
     title: 'JavaScript',
     description: 'Interactive web scripting language',
     button: {
       text: 'Quiz',
       link: '../pages/quiz.html?id=quiz-n2'
       }
-    }
-    ,{
+    },
+    {
         title: 'Python',
         description: 'Easy, versatile coding language',
         button: {
           text: 'Quiz',
           link: '../pages/quiz.html?id=quiz-n3'
         }
-      },
-      {
+    },
+    {
         title: 'HTML',
         description: 'Web page structure language',
         button: {
           text: 'Quiz',
           link: '../pages/quiz.html?id=quiz-n4'
         }
-      },
+    },
+    {
+        title: 'CSS',
+        description: 'Styles and designs for web pages',
+        button: {
+          text: 'Quiz',
+          link: '../pages/quiz.html?id=quiz-n5'
+        }
+    },
+    {
+        title: 'SQL',
+        description: 'Database querying and management',
+        button: {
+          text: 'Quiz',
+          link: '../pages/quiz.html?id=quiz-n6'
+        }
+    }
   ];
