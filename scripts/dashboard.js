9c04d7a08dfd92a5342f525c9373962230734510
Karen Chahrour <karenchahrour123@gmail.com>
2025-04-21 22:25:44 +0300

    Append the data for each row

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -10,6 +10,7 @@ const tableBody = document.getElementById("users-tbody");
 for (let i = 0; i < users.length; i++) {
     const user = users[i];
 
+    if (user.email === "admin@quiz.com") continue;//to skip the admin
     const row = document.createElement("tr");//creating each row in the body
     const indexCell = document.createElement("td"); // Creating data inside the table
     indexCell.textContent = i;
@@ -19,4 +20,24 @@ for (let i = 0; i < users.length; i++) {
 
     const emailCell = document.createElement("td");
     emailCell.textContent = user.email;
+
+    const scoresCell = document.createElement("td");
+
+    const scores = user.scores || {};
+    if (Object.keys(scores).length === 0) { //since scores is an object
+      scoresCell.innerHTML = "No quizzes attempted";
+    } else {
+      let scoresText = "";
+      for (let quizid in scores) {
+        scoresText += `${quizid} => ${scores[quizid]}<br>`;
+      }
+      scoresCell.innerHTML = scoresText;
+    }
+  
+    row.appendChild(indexCell);
+    row.appendChild(nameCell);
+    row.appendChild(emailCell);
+    row.appendChild(scoresCell);
+  
+    tableBody.appendChild(row);
 }
