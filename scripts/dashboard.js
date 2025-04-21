942f6dfd7f88ce797b8c27e6a4cbed32076987a9
Karen Chahrour <karenchahrour123@gmail.com>
2025-04-21 21:47:00 +0300

    Checking is the logged in user is an Admin if not redirect him/her to authentication page

---
+++ scripts/dashboard.js
@@ -0,0 +1,5 @@
+const loggedInUser = JSON.parse(localStorage.getItem("loggedinuser"));
+if (!loggedInUser || loggedInUser.email !== "admin@quiz.com") {
+  alert("Access denied. Admins only.");
+  window.location.href = "../pages/authentication.html";
+}
