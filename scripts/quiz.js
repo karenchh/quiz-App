ee54b31a7bc1e1b26087889443965390a705bc14
Karen Chahrour <karenchahrour123@gmail.com>
2025-04-21 19:00:49 +0300

    Reaching the quizid to know which quiz the user is doing

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -4,3 +4,6 @@ if (!user) {
   window.location.href = "../pages/authentication.html"; // redirect if not logged in
 }
 //console.log(user);
+const quiznumber = new URLSearchParams(window.location.search); // to bring it from the url
+const quizid = quiznumber.get("id");
+console.log(quizid)
